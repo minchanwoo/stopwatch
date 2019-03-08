957c65b8f9f8381b3a43e907f3bb787340019394
dearmcw <dearmcw0070@gmail.com>
2019-03-08 11:30:03 +0900

    스탑워치가 최대 5분까지만 작동하게 if문으로 설정

--- src/App.js
+++ src/App.js
@@ -11,15 +11,17 @@ class App extends Component {
   componentDidMount() {
 
     setInterval(() => {
-      if(this.state.sec === 15) {
-        this.setState({
-          sec: 0,
-          min: this.state.min + 1,
-        });
-      } else {
-        this.setState({
-          sec: this.state.sec + 1
-        });
+      if(this.state.min < 5) {
+        if(this.state.sec === 15) {
+          this.setState({
+            sec: 0,
+            min: this.state.min + 1,
+          });
+        } else {
+          this.setState({
+            sec: this.state.sec + 1
+          });
+        }
       }
     }, 250);
   }
@@ -27,17 +29,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.state.min}:
-        {this.renderSec()}초
+        {this.renderTime(this.state.min)}:
+        {this.renderTime(this.state.sec)}초
       </div>
     );
   }
 
-  renderSec() {
-    if(this.state.sec < 10) {
-      return `0${this.state.sec}`
+  renderTime(time) {
+    if(time < 10) {
+      return `0${time}`
     } else {
-      return this.state.sec
+      return time
     }
   }
 }
