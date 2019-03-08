09ff48320bd969a028fc026b63c9b41eff265798
dearmcw <dearmcw0070@gmail.com>
2019-03-08 11:05:35 +0900

    1초단위로 움직이는초 만들기

--- src/App.js
+++ src/App.js
@@ -5,40 +5,21 @@ import './App.css';
 class App extends Component {
   state = {
     sec: 0,
-    min: 0,
   }
 
   componentDidMount() {
+
     setInterval(() => {
-      if(this.state.min < 3) {
-        if(this.state.sec === 15) {
-          this.setState({
-            sec: 0,
-            min: this.state.min +1,
-          });   
-        } else {
-          this.setState({
-            sec: this.state.sec + 1,
-          });
-        }
-
-      }
-    },250);
-  }
-
-  renderTime(time) {
-    if(time < 10) {
-      return `0${time}`
-    }else {
-      return time
-    }
+      this.setState({
+        sec: this.state.sec +1
+      });
+    }, 1000);
   }
 
   render() {
     return (
       <div className="App">
-        {this.renderTime(this.state.min)}:
-        {this.renderTime(this.state.sec)}초
+        {this.state.sec}초
       </div>
     );
   }
